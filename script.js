330fa133ce26861f68415888c8d63d79896c175b
Mitansh05 <mitansh341@gmail.com>
2026-06-11 10:24:21 -0400

    Finished

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 let paragraph = document.querySelector("p");
 let img = document.querySelector("img");
 
-
 // 2. Remove the comment backslashes from the code below. What happened?
-// paragraph.innerHTML = "Here's a photo of a bear.";
-// img.src = "https://bearwise.org/wp-content/uploads/2022/09/the-over-the-shoulder-pose-scaled-736x414.jpg"; 
+// - The img showed up and the same happened with the <p> tag.
+
+paragraph.innerHTML = "Here's a photo of a bear.";
+img.src =
+  "https://bearwise.org/wp-content/uploads/2022/09/the-over-the-shoulder-pose-scaled-736x414.jpg";
